bdf8878d72cc6bde1c447ce3ed5a2b8d3d861a07
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2022-02-10 19:53:44 +0530

    bugs fixed

--- mobile/lib/widgets/NodesGridView.dart
+++ mobile/lib/widgets/NodesGridView.dart
@@ -12,34 +12,39 @@ class NodesGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 400,
-        childAspectRatio: 2 / 1,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  if (data[index]['node']['isItem'] == null || data[index]['node']['isItem'] == false ) {
-                    return FacilityCategory(data[index]);
-                  }
-                   return Item(itemId: data[index]['node']['_id']);
-                } ,
-              ),
-            );
-          },
-          splashColor: Theme.of(context).primaryColor,
-          child: NodeView(data?[index]),
-        );
-      },
-      itemCount: data?.length ?? 0,
-    );
+    return data?.length != 0
+        ? GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 2 / 1,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        if (data[index]['node']['isItem'] == null ||
+                            data[index]['node']['isItem'] == false) {
+                          return FacilityCategory(data[index]);
+                        }
+                        return Item(itemId: data[index]['node']['_id']);
+                      },
+                    ),
+                  );
+                },
+                splashColor: Theme.of(context).primaryColor,
+                child: NodeView(data?[index]),
+              );
+            },
+            itemCount: data?.length ?? 0,
+          )
+        : const Center(
+            child: Text('No nodes found'),
+          );
   }
 }
