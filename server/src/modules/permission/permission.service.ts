c8dd1ea3b749f13cfb7202383e2aa2085febd2fd
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2022-02-27 20:35:20 +0530

    permission basic logic done

--- server/src/modules/permission/permission.service.ts
+++ server/src/modules/permission/permission.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Permission, PermissionDocument } from './entities/permission.entity';
 import { NodeService } from '../node/node.service';
+import { CreatePermissionInput } from './dto/create-permission.input';
 
 @Injectable()
 export class PermissionService {
@@ -11,6 +12,13 @@ export class PermissionService {
     private permissionModel: Model<PermissionDocument>,
     @Inject(forwardRef(() => NodeService)) private nodeService: NodeService,
   ) {}
+
+  async create(userId: string, nodeId: string) {
+    const createNodeInput = new CreatePermissionInput(userId, nodeId);
+    const PermissionDocument = new this.permissionModel(createNodeInput);
+    const permission = await PermissionDocument.save();
+    return permission._id;
+  }
 
   permissionCheck(userId: string, nodeId: string) {
     return this.permissionModel
