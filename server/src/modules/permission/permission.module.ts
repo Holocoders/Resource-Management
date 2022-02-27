a205cf34eb792c21795d9cafe3404d5d62c94109
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2022-02-27 21:08:07 +0530

    bugs fixed

--- server/src/modules/permission/permission.module.ts
+++ server/src/modules/permission/permission.module.ts
@@ -1,14 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { PermissionResolver } from './permission.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Permission, PermissionSchema } from './entities/permission.entity';
+import { NodeModule } from '../node/node.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Permission.name, schema: PermissionSchema },
     ]),
+    forwardRef(() => NodeModule),
   ],
   providers: [PermissionResolver, PermissionService],
   exports: [PermissionService],
--- server/src/modules/permission/permission.resolver.ts
+++ server/src/modules/permission/permission.resolver.ts
@@ -1,7 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Resolver } from '@nestjs/graphql';
 import { PermissionService } from './permission.service';
+import { CreatePermissionInput } from './dto/create-permission.input';
 
 @Resolver()
 export class PermissionResolver {
   constructor(private readonly permissionService: PermissionService) {}
+
+  async createPermission(
+    @Args('createFacilityInput') createPermissionInput: CreatePermissionInput,
+  ) {
+    return this.permissionService.create(
+      createPermissionInput.userId,
+      createPermissionInput.nodeId,
+    );
+  }
 }
